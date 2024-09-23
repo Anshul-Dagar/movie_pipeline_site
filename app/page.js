cbf6a5d69282bbb99afab08f1858dc0fcb89b982
Anshul <53268875+Anshul-Dagar@users.noreply.github.com>
2024-09-23 10:30:41 +0000

    Background Updated

--- app/page.js
+++ app/page.js
@@ -21,8 +21,8 @@ export default function Home() {
   }, []);
   
   return (
-    <main className="bg-gradient-to-r from-blue-900 via-purple-800 to-red-600 min-h-screen flex flex-col items-center p-10">
-  <h1 className="bg-gray-700 shadow-lg py-4 w-full text-center rounded-lg font-happy-monkey text-3xl md:text-3xl font-bold text-white leading-tight tracking-wide">MOVIE SAGA</h1>
+    <main className="min-h-screen flex flex-col items-center px-8 py-4">
+  <h1 className="bg-[#542437] shadow-lg py-4 w-full text-center rounded-lg font-happy-monkey text-3xl md:text-3xl font-bold text-white leading-tight tracking-wide">MOVIE SAGA</h1>
   <div className={isLoading ? "flex justify-center items-center min-h-screen": "grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10 mt-5"}>
       {isLoading ? <Loader /> : data.map(data =>  (
         <ImageCard key={data.poster_path} path={data.poster_path} />
